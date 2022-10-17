977fc5fc6f96e88443b0f6206bad5a9d13a8e464
dk7696822 <dk7696822@gmail.com>
2022-10-17 18:18:56 +0530

    Assignment on API

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,43 +1,117 @@
-const express = require('express');
-const abc = require('../introduction/intro')
+const express = require("express");
+const abc = require("../introduction/intro");
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    console.log('My batch is', abc.name)
-    abc.printName()
-    res.send('My second ever api!')
+router.get("/test-me", function (req, res) {
+  console.log("My batch is", abc.name);
+  abc.printName();
+  res.send("My second ever api!");
 });
 
-router.get('/students', function (req, res){
-    console.log("The path params in the request are : ", req.params)
-    let students = ['Sabiha', 'Neha', 'Akash']
-    res.send(students)
-})
-
+router.get("/students", function (req, res) {
+  console.log("The path params in the request are : ", req.params);
+  let students = ["Sabiha", "Neha", "Akash"];
+  res.send(students);
+});
 
 // Example 1 for path params
-router.get('/students/:studentName', function(req, res){
-    // ':' denotes that the following part of route is a variable
-    // The value of this variable is what we are sending in the request url after /students
-    // This value is set in the form of an object inside req.params
-    // The object contain key value pairs
-    // key is the variable in the route
-    // value is whatever dynamic value sent in the request url
-    let myParams = req.params
+router.get("/students/:studentName", function (req, res) {
+  // ':' denotes that the following part of route is a variable
+  // The value of this variable is what we are sending in the request url after /students
+  // This value is set in the form of an object inside req.params
+  // The object contain key value pairs
+  // key is the variable in the route
+  // value is whatever dynamic value sent in the request url
+  let myParams = req.params;
 
-    // params attribute is fixed in a request object
-    // params contains the path parameters object
-    console.log("The path params in the request are : ", myParams)
-    res.send('The full name is ' + myParams.studentName )
-})
+  // params attribute is fixed in a request object
+  // params contains the path parameters object
+  console.log("The path params in the request are : ", myParams);
+  res.send("The full name is " + myParams.studentName);
+});
 
 // Example 2 for path params
-router.get('/student-details/:name', function(req, res){
-    let requestParams = req.params
-    console.log("This is the request ", requestParams)
-    let studentName = requestParams.name
-    console.log('Name of the student is ', studentName)
-    res.send('Dummy response')
-})
+router.get("/student-details/:name", function (req, res) {
+  let requestParams = req.params;
+  console.log("This is the request ", requestParams);
+  let studentName = requestParams.name;
+  console.log("Name of the student is ", studentName);
+  res.send("Dummy response");
+});
 
+//Assignment on API
+//Part 1
+router.get("/movies", (req, res) => {
+  let listOfMovies = [
+    "Rang de basanti",
+    "The shining",
+    "Lord of the rings",
+    "Batman begins",
+  ];
+  res.send(listOfMovies);
+});
+//Part 2
+router.get("/movies/:indexNumber", (req, res) => {
+  let listOfMovies = [
+    "Rang de basanti",
+    "The shining",
+    "Lord of the rings",
+    "Batman begins",
+  ];
+  if (listOfMovies[req.params.indexNumber]) {
+    res.send(listOfMovies[req.params.indexNumber]);
+  } else {
+    res.status(400).send("Invalid Index");
+  }
+});
+//Part 3
+router.get("/films", (req, res) => {
+  let listOfAllFilms = [
+    {
+      id: 1,
+      name: "The Shining",
+    },
+    {
+      id: 2,
+      name: "Incendies",
+    },
+    {
+      id: 3,
+      name: "Rang de Basanti",
+    },
+    {
+      id: 4,
+      name: "Finding Nemo",
+    },
+  ];
+  res.send(listOfAllFilms);
+});
+router.get("/films/:filmsID", (req, res) => {
+  let listOfAllFilms = [
+    {
+      id: 1,
+      name: "The Shining",
+    },
+    {
+      id: 2,
+      name: "Incendies",
+    },
+    {
+      id: 3,
+      name: "Rang de Basanti",
+    },
+    {
+      id: 4,
+      name: "Finding Nemo",
+    },
+  ];
+
+  console.log(req.params);
+  const filmByID = listOfAllFilms.find((el) => el.id == req.params.filmsID);
+  if (filmByID) {
+    res.send(filmByID);
+  } else {
+    res.status(400).send("No movie exist with this ID");
+  }
+});
 module.exports = router;
